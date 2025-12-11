8f87517ee15e0ef5f1e54c212e988073c59492d1
Boris Fritscher <boris@fritscher.ch>
2025-12-11 08:23:13 +0100

    implement github actions #13

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ mode }) => ({
+  plugins: [vue(), vueDevTools()],
+  base: mode === 'production' ? '/lia-bfritscher/' : '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
+
